9cf7500eed70cc03f8d0b60613c6fec13080e99f
Taaku0405 <rakosutaa@gmail.com>
2022-08-05 15:46:02 +0900

    ４章完了

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "Seo Takumu";
+		System.out.println(str);
+		str = "思い立ったら即行動";
+		final int LUCKY_NUMBER = 9;
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
