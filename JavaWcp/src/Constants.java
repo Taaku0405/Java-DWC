b2104fad5c979d8b97bf0dd79ace2147dd75bafc
Taaku0405 <rakosutaa@gmail.com>
2022-08-18 18:18:02 +0900

    応用1章完了

---
+++ JavaWcp/src/Constants.java
@@ -0,0 +1,16 @@
+// 定数クラス
+public class Constants {
+
+	// 内部クラスCatの中に、猫関係の定数を定義
+	public static class Cat {
+		public static final String CAT_NAME = "猫";
+		public static final int CAT_LEG_COUNT = 4;
+	}
+
+	// 内部クラスOctopusの中に、タコ関係の定数を定義
+	public static class Octopus {
+		public static final String OCTOPUS_NAME = "タコ";
+		public static final int OCTOPUS_LEG_COUNT = 8;
+	}
+
+}
